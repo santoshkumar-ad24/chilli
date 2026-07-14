c5e74bf0abb11e46972d1a1487e67a251f9f750f
santoshkumar-ad24 <santoshkumar.ad24@bmsce.ac.in>
2026-07-14 20:07:50 +0530

    home

--- chilli_app/public/js/home.js
+++ chilli_app/public/js/home.js
@@ -11,6 +11,7 @@ const observerOptions = {
     rootMargin: "0px 0px -50px 0px"
 };
 
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
